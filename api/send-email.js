e6d3a5e4502749a47f8f4282369f0a187f2ad6d9
Derek Emmanuel <derekemmanuel99@gmail.com>
2024-12-18 04:43:45 +0100

    new

--- api/send-email.js
+++ api/send-email.js
@@ -2,7 +2,7 @@ const nodemailer = require("nodemailer");
 const cors = require("cors");
 
 const corsMiddleware = cors({
-  origin: ["https://exesenergywebsite.vercel.app/", "https://exesenergy.co"],
+  origin: ["https://exesenergywebsite.vercel.app/", "https://exesenergy.co", "https://www.exesenergy.co/contact"],
   methods: ["POST"],
 });
 
