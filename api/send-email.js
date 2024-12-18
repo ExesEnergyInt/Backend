e7d4768487dbd5c7ca14f710f03169c2c020c4ca
Derek Emmanuel <derekemmanuel99@gmail.com>
2024-12-18 03:41:19 +0100

    latest commit

--- api/send-email.js
+++ api/send-email.js
@@ -1,67 +1,48 @@
-const express = require("express");
 const nodemailer = require("nodemailer");
-const cors = require("cors");
-require("dotenv").config();
 
-const app = express();
-app.use(express.json());
-app.use(cors());
-
-let transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    type: "OAuth2",
-    user: process.env.EMAIL_HOST,
-    pass: process.env.HOST_PASSWORD,
-    clientId: process.env.Client_ID,
-    clientSecret: process.env.Client_Secret,
-    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-  },
-});
-
-// Verify transporter
-transporter.verify((err, success) => {
-  if (err) {
-    console.error("Transporter verification failed:", err);
-  } else {
-    console.log("=== Server is ready to take messages ===");
+module.exports = async (req, res) => {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
   }
-});
 
-// Route to send emails
-app.post("/api/send-email", (req, res) => {
   const { name, email, subject, message } = req.body;
 
+  // Input validation
   if (!name || !email || !subject || !message) {
     return res
       .status(400)
       .json({ status: "fail", error: "All fields are required." });
   }
 
-  const mailOptions = {
-    from: email,
-    to: process.env.EMAIL_RECIEVE,
-    subject: `Message from ${name} (${email}): ${subject}`,
-    text: message,
-  };
+  // Nodemailer configuration
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: process.env.EMAIL_HOST,
+      pass: process.env.HOST_PASSWORD,
+      clientId: process.env.Client_ID,
+      clientSecret: process.env.Client_Secret,
+      refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+    },
+  });
 
-  transporter.sendMail(mailOptions, (err) => {
-    if (err) {
-      console.error("Error sending email:", err);
-      return res
-        .status(500)
-        .json({ status: "fail", error: "Failed to send email." });
-    }
+  try {
+    // Send mail
+    await transporter.sendMail({
+      from: email,
+      to: process.env.EMAIL_RECIEVE,
+      subject: `Message from ${name} (${email}): ${subject}`,
+      text: message,
+    });
 
-    console.log("== Message Sent ==");
-    res
+    return res
       .status(200)
       .json({ status: "success", message: "Email sent successfully." });
-  });
-});
-
-
-const port = 5000;
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
+  } catch (error) {
+    console.error("Error sending email:", error);
+    return res
+      .status(500)
+      .json({ status: "fail", error: "Failed to send email." });
+  }
+};
